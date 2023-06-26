278c301aa909df62434a201063e33a8f4bd030d0
Emma Gerigscott <elgerig@gmail.com>
2023-06-26 15:05:02 -0700

    update profile nav
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ProfileInfo from "./ProfileInfo";
-import ProfileNav from "./ProfileNav";
 import ProfilePosts from "./ProfilePosts";
 import ranger from './../img/rangerdanger.gif';
 
@@ -17,7 +16,6 @@ function Profile(){
         <ProfileInfo userPic={userProfile.userPic}
         user={userProfile.user}
         bio={userProfile.bio}/>
-        <ProfileNav />
         <ProfilePosts />
     </div>
   );
