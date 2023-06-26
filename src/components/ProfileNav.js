9db4ca8740f1b626ba437a4f68cc6b933e1058f9
Emma Gerigscott <elgerig@gmail.com>
2023-06-26 11:55:08 -0700

    create profileNav component
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

---
+++ src/components/ProfileNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProfileNav() {
+  return (
+    <div>
+      <button>Posts</button>
+      <button>Followers</button>
+      <button>Following</button>
+    </div>
+  );
+}
+
+export default ProfileNav;
