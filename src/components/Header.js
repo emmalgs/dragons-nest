8940d8fec79b650041bde3675887ee66731f00fe
Emma Gerigscott <elgerig@gmail.com>
2023-06-26 15:06:09 -0700

    add class name
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return (
         <React.Fragment>
-            <h1>Dragon's Nest</h1>
+            <h1 className="header">Dragon's Nest</h1>
         </React.Fragment>
     );
 }
